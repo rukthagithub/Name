d518703ad7a9e4d5cd7d33c84dcd6b0c13387ab0
Vijay Thakur <105146485+rukthagithub@users.noreply.github.com>
2022-11-09 21:26:38 +0530

    Add files via upload

---
+++ Name_Intern_Engineer.pdf/Third.java
@@ -0,0 +1,38 @@
+
+
+public class Third {
+    
+    public static void main(String args[]) {
+        System.out.println("\n");
+        System.out.println("$arr = [4,6,3,1,5]");
+        System.out.println("\n");
+        for (int i =0; i<4; i++){
+            System.out.println("*");
+        }
+        System.out.println("\n");
+        for (int j = 0; j<8; j++){
+            System.out.println("*");
+          
+        }
+        
+        System.out.println("\n");
+        for (int K = 0; K<3; K++){
+            
+            System.out.println("*");
+           
+        }
+        System.out.println("\n");
+        for (int L = 0; L<1; L++){
+            System.out.println("*");
+          
+        }
+        System.out.println("\n");
+        for (int M = 0; M<5; M++){
+            System.out.println("*");
+          
+        }
+    }
+}
+
+
+
